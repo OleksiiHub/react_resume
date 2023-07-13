71039d86d8a6f3f79028549f4e8e5ba93f3ac22c
OLEKSII BUNTIKOV <githubbai@gmail.com>
2023-07-13 16:51:01 +0300

    add skills, style li

--- src/components/ExpDescription.js
+++ src/components/ExpDescription.js
@@ -1,13 +1,37 @@
 function ExpDescription() {
     return (
+
         <div className="description-style">
             <h3 className="description-style_header header-style mar_h3_h4_h5">Summary</h3>
-            <p className="description-style_comment">I am a beginner frontend developer. I completed courses in this area, I am engaged in self-study. In the future, I plan to get to know Node.js, Express.js, Nest.js, Koa.js and MongoDB better. At the moment I know html5, css3, JavaScript, React. I am learning Redux, Git, preprocessors less, sass, scss. I use the libraries ReactBootstrap , Tailwind.</p>
+            <p className="description-style_comment">I am a beginner frontend developer. 
+            I completed courses in this area, I am engaged in self-study. 
+            In the future, I plan to get to know Node.js, Express.js, Nest.js, Koa.js and MongoDB better. 
+            At the moment I know html5, css3, JavaScript, React, Git, preprocessors less, sass, scss. I am learning Redux. 
+            I use the libraries ReactBootstrap , Tailwind.</p>
+            <h3 className="description-style_header header-style mar_h3_h4_h5">Skills Highlights</h3>
+            <div class="style_skills mar_h3_h4_h5">
+                <div class="col-50">
+                    <ul>
+                        <li> HTML5</li>
+                        <li> CSS3</li>
+                        <li> JavaScript</li>
+                        <li> React</li>
+                    </ul>
+                </div>
+                <div class="col-50">
+                    <ul>
+                        <li> Bootstrap</li>
+                        <li> Git</li>
+                        <li> SASS SCSS LESS</li>
+                        <li> Redux</li>
+                    </ul>
+                </div>
+            </div>
+            <h3 className="description-style_header header-style mar_h3_h4_h5">Work Experience</h3>
+            <p className="description-style_comment"> null</p>
+
         </div>
-
     )
-
-
 }
 
 export default ExpDescription
