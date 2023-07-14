870a99d1fc8000579d90da8c8a9565bbd30b79f8
OLEKSII BUNTIKOV <githubbai@gmail.com>
2023-07-14 16:26:40 +0300

    add button

--- src/components/ExpDescription.js
+++ src/components/ExpDescription.js
@@ -6,7 +6,7 @@ function ExpDescription() {
                 I completed courses in this area, I am engaged in self-study.
                 In the future, I plan to get to know Node.js, Express.js, Nest.js, Koa.js and MongoDB better.
                 At the moment I know html5, css3, JavaScript, React, Git, preprocessors less, sass, scss. I am learning Redux.
-                I use the libraries ReactBootstrap , Tailwind.</p>
+                I use the libraries ReactBootstrap , Tailwind and others.</p>
             <h3 className="description-style_header header-style mar_h3_h4_h5">Skills Highlights</h3>
             <div class="description-style_skills mar_h3_h4_h5">
                 <div class="col-50">
@@ -49,10 +49,16 @@ function ExpDescription() {
                 <p className="description-style_comment"><span className="description-style_experience">Dates of studying:</span> c 2022 – по 2022</p>
                 <p className="description-style_comment"><span className="description-style_experience">Name of institution:</span> PROG_academy</p>
                 <p className="description-style_comment"><span className="description-style_experience">Degree and major</span> JavaScript developer</p>
-            </div>            <div className="description-style_border">
+            </div>
+            <div className="description-style_border">
                 <p className="description-style_comment"><span className="description-style_experience">Dates of studying:</span>c 2022</p>
                 <p className="description-style_comment"><span className="description-style_experience">Name of institution:</span> Udemy(Bogdan Stashchuk, Ivan Petrychenko)</p>
                 <p className="description-style_comment"><span className="description-style_experience">Degree and major</span> WEB, HTML5, CSS3, JavaScript, React, Git, Redux</p>
+            </div>
+            <div className="description-style_border">
+                <button className="btn description-style_btn">
+                    Weather App
+                </button>
             </div>
         </div>
     )
