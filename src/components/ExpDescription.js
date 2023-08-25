216d6e138fa9b3ba685972cb303efdc010f7ce0c
OLEKSII BUNTIKOV <githubbai@gmail.com>
2023-08-25 19:48:54 +0300

    fixed style description

--- src/components/ExpDescription.js
+++ src/components/ExpDescription.js
@@ -1,8 +1,11 @@
 function ExpDescription() {
     return (
         <div className="description-style">
+            <h3 className="description-style_header header-style mar_h3_h4_h5">The position I am applying for</h3>
+            <p className="description-style_header mar_h3_h4_h5">Junior Frontend Developer</p>
+
             <h3 className="description-style_header header-style mar_h3_h4_h5">Summary</h3>
-            <p className="description-style_header mar_h3_h4_h5">I am the begining Front end Developer.
+            <p className="description-style_header mar_h3_h4_h5">I am the begining Frontend Developer.
                 The desire to learn and self-education are my priorities. That is why I have completed several courses in order to know  html5, css3, JavaScript, Reacr, Git, preprocessors LESS, SASS, and SCSS. My next step will be farmilization with Node.js, Express.js, Nest,js, Koa.js and MongoBD. Cerruntly i am learning Redux. I use libraries ReactBootstrap, Tailwind etc.</p>
             <h3 className="description-style_header header-style mar_h3_h4_h5">Skills Highlights</h3>
             <div className="description-style_skills mar_h3_h4_h5">
@@ -26,13 +29,13 @@ function ExpDescription() {
             <h3 className="description-style_header header-style mar_h3_h4_h5">Work Experience</h3>
             <div className="description-style_border">
                 <p className="description-style_comment"><span className="description-style_experience">Name of employer:</span> Khladoprom Ice Cream Factory(Kharkiv, Mariupol)</p>
-                <p className="description-style_comment"><span className="description-style_experience">Dates of employment:</span> 2019-2021</p>
+                <p className="description-style_comment"><span className="description-style_experience">Dates of employment:</span> from 2017 to 2021</p>
                 <p className="description-style_comment"><span className="description-style_experience">Job title:</span> Head of trade department</p>
                 <p className="description-style_comment"><span className="description-style_experience">Project/Role description:</span> Recruitment and training of personnel, product promotion, contracting, marketing, advertising, logistics, receivables control, reporting.</p>
             </div>
             <div className="description-style_border">
                 <p className="description-style_comment"><span className="description-style_experience">Name of employer:</span> Viciunai Group(Kharkiv, Kharkiv region)</p>
-                <p className="description-style_comment"><span className="description-style_experience">Dates of employment:</span> 2021-2022</p>
+                <p className="description-style_comment"><span className="description-style_experience">Dates of employment:</span> from 2021 to 2022</p>
                 <p className="description-style_comment"><span className="description-style_experience">Job title:</span> Territorial manager</p>
                 <p className="description-style_comment"><span className="description-style_experience">Project/Role description:</span> Conclusion of contracts, marketing, control of receivables, connection of distributors, training of the sales team of the distributor, search for new sales channels.</p>
             </div>
