28a058b6a2c5ce84cbd8a3bb1297181d3b8edbe6
OLEKSII BUNTIKOV <githubbai@gmail.com>
2023-09-11 18:17:56 +0300

    add resume file, fixed style

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,21 +9,21 @@ function AboutMe({ src, target, alt }) {
             <div className='text-center'>
                 <img className="about-style_img" src={src} alt={alt} />
             </div>
-            <h3 className="header-style mar_h3_h4_h5">Contacts</h3>
-            <h4 className='mar_h3_h4_h5'>{<Icon name='phone square' />}Phone</h4>
-            <h5 className='mar_h3_h4_h5'><a href="tel:+380660075737">+38 (006) 007-57-37</a></h5>
-            <h4 className='mar_h3_h4_h5'>{<Icon name='mail' />}E-mail</h4>
-            <h5 className='mar_h3_h4_h5'><a href="mailto:githubbai@gmail.com">githubbai@gmail.com</a></h5>
-            <h4 className='mar_h3_h4_h5'>{<Icon name='linkedin' />}Linkedin</h4>
-            <h5 className='mar_h3_h4_h5'><a target={target} href="https://www.linkedin.com/in/okelsii-buntikov-131038187"
+            <h3 className="header-style margin-style">Contacts</h3>
+            <h4 className='margin-style'>{<Icon name='phone square' />}Phone</h4>
+            <h5 className='margin-style'><a href="tel:+380660075737">+38 (006) 007-57-37</a></h5>
+            <h4 className='margin-style'>{<Icon name='mail' />}E-mail</h4>
+            <h5 className='margin-style'><a href="mailto:githubbai@gmail.com">githubbai@gmail.com</a></h5>
+            <h4 className='margin-style'>{<Icon name='linkedin' />}Linkedin</h4>
+            <h5 className='margin-style'><a target={target} href="https://www.linkedin.com/in/okelsii-buntikov-131038187"
             >View page LinkedIn</a></h5>
-            <h4 className='mar_h3_h4_h5'>{<Icon name='github square' />}GitHub</h4>
-            <h5 className='mar_h3_h4_h5'><a target={target} href="https://github.com/OleksiiHub?tab=repositories">View page GitHub</a></h5>
-            <h4 className='mar_h3_h4_h5'>{<Icon name='location arrow' />}Address</h4>
-            <h5 className='mar_h3_h4_h5'><a target={target} href="https://www.google.com/maps/place/%D0%A5%D0%B0%D1%80%D1%8C%D0%BA%D0%BE%D0%B2,+%D0%A5%D0%B0%D1%80%D1%8C%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C/@49.9945914,36.2858247,11z/data=!3m1!4b1!4m6!3m5!1s0x4127a09f63ab0f8b:0x2d4c18681aa4be0a!8m2!3d49.9935!4d36.230383!16zL20vMDgyc3k5?entry=ttu">Ukraine, Kharkiv</a></h5>
-            <h3 className="header-style mar_h3_h4_h5">Languages</h3>
-            <h5 className='mar_h3_h4_h5'>Ukrainian - Native</h5>
-            <h5 className='mar_h3_h4_h5'>English - Elementary</h5>
+            <h4 className='margin-style'>{<Icon name='github square' />}GitHub</h4>
+            <h5 className='margin-style'><a target={target} href="https://github.com/OleksiiHub?tab=repositories">View page GitHub</a></h5>
+            <h4 className='margin-style'>{<Icon name='location arrow' />}Address</h4>
+            <h5 className='margin-style'><a target={target} href="https://www.google.com/maps/place/%D0%A5%D0%B0%D1%80%D1%8C%D0%BA%D0%BE%D0%B2,+%D0%A5%D0%B0%D1%80%D1%8C%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C/@49.9945914,36.2858247,11z/data=!3m1!4b1!4m6!3m5!1s0x4127a09f63ab0f8b:0x2d4c18681aa4be0a!8m2!3d49.9935!4d36.230383!16zL20vMDgyc3k5?entry=ttu">Ukraine, Kharkiv</a></h5>
+            <h3 className="header-style margin-style">Languages</h3>
+            <h5 className='margin-style'>Ukrainian - Native</h5>
+            <h5 className='margin-style'>English - Elementary</h5>
         </div>
     )
 }
--- src/components/ExpDescription.js
+++ src/components/ExpDescription.js
@@ -1,14 +1,14 @@
 function ExpDescription() {
     return (
         <div className="description-style">
-            <h3 className="description-style_header header-style mar_h3_h4_h5">The position I am applying for</h3>
-            <p className="description-style_header mar_h3_h4_h5">Junior Frontend Developer</p>
+            <h3 className="description-style_header header-style margin-style">Career objective</h3>
+            <p className="description-style_header margin-style">Junior Frontend Developer</p>
 
-            <h3 className="description-style_header header-style mar_h3_h4_h5">Summary</h3>
-            <p className="description-style_header mar_h3_h4_h5">I am the begining Frontend Developer.
+            <h3 className="description-style_header header-style margin-style">Summary</h3>
+            <p className="description-style_header margin-style">I am the begining Frontend Developer.
                 The desire to learn and self-education are my priorities. That is why I have completed several courses in order to know  HTML5, CSS3, JavaScript, React, Git, preprocessors LESS, SASS, and SCSS. My next step will be farmilization with Node.js, Express.js, Nest,js, Koa.js and MongoBD. Cerruntly i am learning Redux. I use libraries React Bootstrap, Tailwind etc.</p>
-            <h3 className="description-style_header header-style mar_h3_h4_h5">Skills Highlights</h3>
-            <div className="description-style_skills mar_h3_h4_h5">
+            <h3 className="description-style_header header-style margin-style">Skills Highlights</h3>
+            <div className="description-style_skills margin-style">
                 <div className="col-50">
                     <ul>
                         <li> HTML5</li>
@@ -26,7 +26,7 @@ function ExpDescription() {
                     </ul>
                 </div>
             </div>
-            <h3 className="description-style_header header-style mar_h3_h4_h5">Work Experience</h3>
+            <h3 className="description-style_header header-style margin-style">Work Experience</h3>
             <div className="description-style_border">
                 <p className="description-style_comment"><span className="description-style_experience">Name of employer:</span> Khladoprom Ice Cream Factory(Kharkiv, Mariupol)</p>
                 <p className="description-style_comment"><span className="description-style_experience">Dates of employment:</span> from 2017 to 2021</p>
@@ -39,7 +39,7 @@ function ExpDescription() {
                 <p className="description-style_comment"><span className="description-style_experience">Job title:</span> Territorial manager</p>
                 <p className="description-style_comment"><span className="description-style_experience">Project/Role description:</span> Conclusion of contracts, marketing, control of receivables, connection of distributors, training of the sales team of the distributor, search for new sales channels.</p>
             </div>
-            <h3 className="description-style_header header-style mar_h3_h4_h5">Education</h3>
+            <h3 className="description-style_header header-style margin-style">Education</h3>
             <div className="description-style_border">
                 <p className="description-style_comment"><span className="description-style_experience">Dates of studying:</span> from 2022 – to 2023</p>
                 <p className="description-style_comment"><span className="description-style_experience">Name of institution:</span> IT_Generation robotdreams</p>
@@ -58,7 +58,7 @@ function ExpDescription() {
             <div className="description-style_border">
                 <a className="btn description-style_btn mb-1" href="http://localhost:3000/">Resume</a>
                 <a className="btn description-style_btn mb-1" href="http://localhost:3000/">React Weather App</a>
-                <a className="btn description-style_btn mb-1" href="http://localhost:3000/">Download Resume</a>
+                <a className="btn description-style_btn mb-1" href="../files/resume.pdf" download>Download Resume</a>
             </div>
         </div>
     )
