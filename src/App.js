a685a1f6bc4c74260b83c4fc140bfd29e9a0f24d
OLEKSII BUNTIKOV <githubbai@gmail.com>
2024-07-21 19:24:12 +0300

    add picture

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './css/style.min.css'
-import './css/app.min.css'
-import './css/getweeather.min.css'
+import './css/style.css'
+import './css/app.css'
+import './css/getweeather.css'
 import Resume from './components/Resume'
-import GetWeather  from './components/GetWeather'
+import GetWeather from './components/GetWeather'
 
 function App() {
   return (
